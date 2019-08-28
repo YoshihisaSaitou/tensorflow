14dfb75a04d0b245513ef529856410755dd5a119
Yoshihisa Saito <anti_viral_drug@yahoo.co.jp>
2019-08-28 17:38:14 +0900

    Update camera.py
    カスケードファイルの顔認識追加

--- camera.py
+++ camera.py
@@ -21,12 +21,29 @@ canvas.pack()
 # デフォルトでは0、ノートPCの内臓Webカメラは0、別にUSBカメラを接続した場合は1を入れる。
 cap = cv2.VideoCapture(0)
 
+cascade_file = "haarcascade_frontalface_default.xml"
+
 def show_frame():
     global CANVAS_X, CANVAS_Y
 
     ret, frame = cap.read()
     if ret == False:
         print('カメラから画像を取得できませんでした')
+    
+    # グレースケールに変換
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # カスケードファイルを利用して顔の位置を見つける
+    cascade = cv2.CascadeClassifier(cascade_file)
+    face_list = cascade.detectMultiScale(gray, minSize=(100, 100))
+    
+    for (x, y, w, h) in face_list:
+        print("face_position:",x, y, w, h)
+        color = (0, 0, 255)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color, thickness = 5)
+    
+    #cv2.imshow('frame', frame)
+    
     # BGRなのでRGBに変換
     image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     # RGBからPILフォーマットへ変換
@@ -37,7 +54,9 @@ def show_frame():
     # そのために下のようにインスタンスを作っておくかグローバル変数にしておく
     canvas.image_tk = image_tk
     # global image_tk
-
+    
+    
+    
     # ImageTk 画像配置　画像の中心が指定した座標x,yになる
     canvas.create_image(CANVAS_X / 2, CANVAS_Y / 2, image=image_tk)
     # Canvasに現在の日時を表示
